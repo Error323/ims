889ab1bc2386e98c13d439f365cf1e615ae4cd17
Astone <astone.git@gmail.com>
2009-09-16 17:06:25 +0200

    main function defined

--- lab1/main.m
+++ lab1/main.m
@@ -8,11 +8,34 @@
 %
 %   See also imsNormalizeRgb imsRgbToOpponent imsRgbToHsv
 
+
+%% Read an image (I) and show it
+
+image = 'data/nemo2.jpg';
+
+I = im2double(imread(image));
+
+figure('Name', 'Source image');
+
+imshow(I);
+
+
 %% RGB to normalized rgb conversion
+
+figure('Name', 'RGB to normalized rgb');
+
+imshow(imsNormalizeRgb(I));
 
 
 %% RGB to Opponent Color Space conversion
 
+figure('Name', 'RGB to Opponent Color Space');
+
+imshow(imsRgbToOpponent(I));
+
 
 %% RGB to HSV conversion
 
+figure('Name', 'RGB to HSV conversion');
+
+imshow(imsRgbToHsv(I));
