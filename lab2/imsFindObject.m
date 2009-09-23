3ab2f8aeb0a9a8c11884f3b5ade49549b676ffb2
Error323 <folkerthuizinga@gmail.com>
2009-09-24 01:32:47 +0200

    Cleanup

--- lab2/imsFindObject.m
+++ lab2/imsFindObject.m
@@ -17,8 +17,8 @@ function [x, y, r, e] = imsFindObject(I, Ho, d, filename)
     [Y,X,Z_] = size(I);
     J = zeros(size(I,1), size(I,2));
     M = imsGaussFilter(d);
-    for i = r+1:2:(Y-r-1)
-        for j = r+1:2:(X-r-1)
+    for i = r+1:5:(Y-r-1)
+        for j = r+1:5:(X-r-1)
             F = I((i-r:i+r), (j-r:j+r), :);
             H = imsHistogram(F, M, size(Ho,1));
             J(i,j) = sum(sum((H - Ho).^2));
@@ -29,8 +29,3 @@ function [x, y, r, e] = imsFindObject(I, Ho, d, filename)
             end
         end
     end
-    J = -J;
-    J = (J - min(J(:))) ./ (max(J(:)) - min(J(:)));
-    figure;imshow(J);
-    imwrite(J, filename);
-    imwrite(J, filename);
